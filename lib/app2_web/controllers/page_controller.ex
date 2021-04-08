a6bca1f69be6624f54fc460042c1c216a7d00b30
MMcClure11 <mcclme02@luther.edu>
2021-04-08 10:28:59 -0500

    Remove unnecessary pages

--- lib/app2_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule App2Web.PageController do
-  use App2Web, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- lib/app2_web/views/page_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule App2Web.PageView do
-  use App2Web, :view
-end
