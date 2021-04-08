bd83503a46d44a43945d7f5b4973ece5428cf93f
MMcClure11 <mcclme02@luther.edu>
2021-04-08 08:00:38 -0500

    Add item_view helper functions and apply to index for status of items

--- lib/app2_web/views/item_view.ex
+++ lib/app2_web/views/item_view.ex
@@ -1,3 +1,19 @@
 defmodule App2Web.ItemView do
   use App2Web, :view
+
+  # add class "completed" to a list item if item.status=1
+  def complete(item) do
+    case item.status do
+      1 -> "completed"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
+
+  # add "checked" to input if item.status=1
+  def checked(item) do
+    case item.status do
+      1 -> "checked"
+      _ -> "" # empty string means empty class so no style applied
+    end
+  end
 end
